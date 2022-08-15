45dd353a864ec919427027c9aa8fa68c70c527d7
ov3rl0rd oficial <111184353+ov3rl0rd-re4pp3r@users.noreply.github.com>
2022-08-14 22:17:39 -0300

    Add files via upload

---
+++ redirect.php
@@ -0,0 +1,12 @@
+<!-- redirecionamento 301 -->
+<?
+Header( “HTTP/1.1 301 Moved Permanently” );
+Header( “Location: chat.whatsapp.com/F9eDoaMcAkc4tPMcUS3Rdj” );
+?>
+
+<!-- redirecionamento 301 -->
+<?
+Header( “
+HTTP/1.1 301 Moved Permanently” );
+Header( “Location: discord.gg/3FV4HmN9” );
+?>
